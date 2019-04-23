9a1ff4193ddb36476aab4bb034129fd1be5183d7
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2019-04-23 17:14:24 +0200

    Servet 4.0 - JSR 369

---
+++ servlet/src/main/java/com/guga/ee/controller/AppMain.java
@@ -0,0 +1,24 @@
+package com.guga.ee.controller;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+public class AppMain {
+
+    public static void main(String... args) throws Exception {
+        Server server = new Server();
+        try (ServerConnector connector = new ServerConnector(server)) {
+            connector.setPort(8090);
+            server.setConnectors(new Connector[]{connector});
+            ServletHandler handler = new ServletHandler();
+            server.setHandler(handler);
+            handler.addServletWithMapping(ServerSentEvent.class, "/ServerPush");
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
