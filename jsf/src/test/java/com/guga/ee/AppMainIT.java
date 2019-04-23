e61c5cecbb6512a81cd7a1a6d5284719c9a0ef2c
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2019-04-23 14:05:04 +0200

    Fix project

--- jsf/src/test/java/com/guga/ee/AppMainIT.java
+++ jsf/src/test/java/com/guga/ee/AppMainIT.java
@@ -1,6 +1,7 @@
 package com.guga.ee;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,13 +10,13 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AppMainTest {
+public class AppMainIT {
 
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
-    void given_a_request_when_page_is_found_then_return_ok() {
+    public void given_a_request_when_page_is_found_then_return_ok() {
         webTestClient.get().uri("/ui/index.xhtml").exchange().expectStatus().isOk();
     }
 
